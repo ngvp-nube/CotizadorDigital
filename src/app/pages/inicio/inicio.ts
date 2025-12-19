6f93ad24beb9059fcfac38d439118ad2405820b1
ngvp-nuve <ni.villagrap@duocuc.cl>
2025-12-19 15:22:57 -0400

    update dinamic

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  trigger,  transition,
+  style,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-inicio',
   imports: [],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
+  animations: [
+    trigger('fadeSlide', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('300ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        )
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in',
+          style({ opacity: 0, transform: 'translateY(10px)' })
+        )
+      ])
+    ])
+  ]
 })
 export class Inicio {
   constructor(private router: Router) {}
